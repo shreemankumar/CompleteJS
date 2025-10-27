298c20f799fc046c020a8721f1a21c363a5a6c89
Shreeman Kumar Badatya <137563531+shreemankumar@users.noreply.github.com>
2025-10-27 23:32:47 +0530

    control flow

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,2 +1,10 @@
-var a = 20;
-a > 30 ? console.log("no") : a <= 20 ? console.log("yes") : console.log("wfhjuj");
+function rps(user,computer){
+  if(user === computer) return "draw";
+
+  if(user === "rock" && computer === "scissor") return "user";
+  if(user === "scissor" && computer === "rock") return "computer";
+  if(user === "paper" && computer === "rock") return "user"
+
+  return "computer";
+}
+console.log(rps("rock","scissor"))
