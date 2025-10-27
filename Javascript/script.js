3d8d6760bfae6014264a0813094a1d429aea11b2
Shreeman Kumar Badatya <137563531+shreemankumar@users.noreply.github.com>
2025-10-27 18:12:19 +0530

    Update script.js
    some basics common question.

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,9 +1,34 @@
-// Hoisting impact per type
-//var a = undefined
+// let a = 12;
+// {
+//   let a = 13;
+//   console.log("Inside :",a);
+// }
+// console.log("outside :",a)
+// // var can not use outside of the function 
+// // let can not be used outside this {} bracket
 
+// if(true){
+//   var s = 1;
+//   let d = 2;
+// }
+// console.log(s);// 1
+// console.log(d);// undefined
 
-console.log(a);  //  undefined
-var a = 12;
-// var a = undefined (this goes to the top)
-// a = 12
+// // in const
+
+// const person = {name : "shreeman"};
+// person.name = "shree"; // update is allowed
+// person = {}; // not allowed 
+// // Assignment to constant variable.
+
+// primitive
+// let a = 12;
+// let b = a;
+//  a = a + 2; // a = 14 , b = 12
+
+// reference
+
+let a = [1,2,4];
+let b = a;
+b.pop();
  
