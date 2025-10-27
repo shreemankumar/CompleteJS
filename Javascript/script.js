d4d20f9d6f6edbb582f62bc3a5faa6ba52af4e06
Shreeman Kumar Badatya <137563531+shreemankumar@users.noreply.github.com>
2025-10-27 13:10:08 +0530

    Initial commit

---
+++ Javascript/script.js
@@ -0,0 +1,10 @@
+// var , let , const
+// declaration and intitalization
+ var a; // declartion
+ var a = 12;  // declaration and initialization
+ 
+ /**
+  *   var:  window main add hota hai
+  *   var:  main function scoped hota hai
+  *   app fir se declare kar sakte hosame name se and koi error nehi aayega 
+  */
