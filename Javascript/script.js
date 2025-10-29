8366b4d73b52998e57c4db4a6177fa5ba7ea1441
Shreeman Kumar Badatya <137563531+shreemankumar@users.noreply.github.com>
2025-10-29 16:46:07 +0530

    Update script.js
    map(),reduce(),some(),every(),destructuring and spread operator.

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,33 +1,65 @@
-// type 1
-let marks = [78,89,90,98,71];
-console.log(marks[2])// 90
-console.log(marks[4])// 71
-console.log(marks[5])// undefined
-// modify
-marks[3] = 44;
-console.log(marks)
-
-marks.push(11);// this 11 will be pushing on the last of the array
-console.log(marks)
-marks.pop()// last element will be deleted from the array
-console.log(marks)
-marks.shift();// remove first element 
-console.log(marks)
-marks.unshift(3) //  add element at the first
-console.log(marks)
-marks.splice(2,1); // where first (2,2) 2 : which index se value koo haata na hai and the second 2 which denotes that kitni value uusse index se haata na hai 
-console.log(marks)
-  let newarray = marks.slice(2);
-//let newarray = marks.slice(0,2);
-console.log(marks)
-console.log(newarray)
-
-marks.sort(function(a,b){
-  // return a - b;// ascending order 
-  return b - a;// descending order 
+let arr = [1,2,3,4,5];
+// arr.forEach(element => {
+//   console.log(element)
+// });
+// arr.forEach(function(val){
+//   console.log(val + 1);
+// })
+// map
+let newarr = arr.map(function(val){
+  return 12;
 })
-marks.reverse();
+// filter
 
-console.log(marks)
-//type 2
-let mark = new Array();
+let nums = [12,11,3,22,55,3];
+let newnums  = nums.filter(function(element){
+  return (element > 10);
+})
+// reduce
+// sum of the ele of an array : reduce the ele of the number in to one....
+let sumsrr = nums.reduce(function(acc,element){
+  return acc + element;
+},0)
+// let findele =  nums.find(function(ele){
+//   return ele === 11
+// })
+
+let findelement =  [
+  {id : 1 , val : 12},
+  {id : 2 , val : 2},
+  {id : 6 , val : 12}
+];
+let findele = findelement.find(function(ele){
+  return ele.val === 12;
+})
+
+let issome = nums.some(function(ele){
+  return ele > 32;
+})
+
+let isevery = nums.every(function(ele){
+  return ele > 1;
+})
+// destructuring and spread operator
+let [a,d,,,e] = nums;
+let nums1 = [...nums]
+/*
+nums
+(6) [12, 11, 3, 22, 55, 3]
+nums1
+(6) [12, 11, 3, 22, 55, 3]
+nums1.pop
+ƒ pop() { [native code] }
+nums1.pop()
+3
+nums1
+(5) [12, 11, 3, 22, 55]
+nums
+(6) [12, 11, 3, 22, 55, 3]
+nums1.pop()
+55
+nums1
+(4) [12, 11, 3, 22]
+nums
+(6) [12, 11, 3, 22, 55, 3]
+*/
