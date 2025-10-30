63e0c708f324956ada3c812899f36cc342449132
Shreeman Badatya <bshreemankumar@gmail.com>
2025-10-30 23:45:57 +0530

    create DOM
    learning about
    Document. =>
    getElementById,getElementsByClassName,querySelector,querySelectorAll

---
+++ DOM/script.js
@@ -0,0 +1,8 @@
+let getid = document.getElementById("id");
+console.dir(getid);
+let getwithoutid = document.getElementsByClassName("abcd"); // in console u will see the technical properties
+console.log(getwithoutid);
+let getwithquerry = document.querySelector("h1");
+console.log(getwithquerry);
+let getwithquerrys = document.querySelectorAll("h1");
+console.log(getwithquerrys);
