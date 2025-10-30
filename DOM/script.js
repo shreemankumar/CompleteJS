ed8c7e3421a5894c4e99a9d64974a8307c62ffc0
Shreeman Badatya <bshreemankumar@gmail.com>
2025-10-31 00:41:51 +0530

    Text/content access
    text modification
    with  :
    -innerText
    -textContent
    -innterHTML (changes the innterhtml from the text and add new text by :
    
    h1.innerHTML = “<i>this text will be in italic sytle</i>)”

--- DOM/script.js
+++ DOM/script.js
@@ -1,8 +1,14 @@
-let getid = document.getElementById("id");
-console.dir(getid);
-let getwithoutid = document.getElementsByClassName("abcd"); // in console u will see the technical properties
-console.log(getwithoutid);
-let getwithquerry = document.querySelector("h1");
-console.log(getwithquerry);
-let getwithquerrys = document.querySelectorAll("h1");
-console.log(getwithquerrys);
+// let getid = document.getElementById("id");
+// console.dir(getid);
+// let getwithoutid = document.getElementsByClassName("abcd"); // in console u will see the technical properties
+// console.log(getwithoutid);
+// let getwithquerry = document.querySelector("h1");
+// console.log(getwithquerry);
+// let getwithquerrys = document.querySelectorAll("h1");
+// console.log(getwithquerrys);
+
+let h1 = document.querySelector("h1");
+// h1.outerText = "I'am Fine";
+//h1.innerText = "I'am Fine";
+h1.innerHTML = "<strong>I'am Fine</strong>";
+console.log(h1);
