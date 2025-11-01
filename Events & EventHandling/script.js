cd8630a7cb4ab0afcda55a4fee582fe08edb6dcd
Shreeman Badatya <bshreemankumar@gmail.com>
2025-11-02 01:12:04 +0530

    input event

--- Events & EventHandling/script.js
+++ Events & EventHandling/script.js
@@ -1,7 +1,7 @@
-// let h1 = document.querySelector("h1");
-// function Whenclick() {
-//   h1.style.color = "red";
-// }
-// //h1.addEventListener("click", Whenclick);
-// h1.addEventListener("click", Whenclick);
-// //h1.removeEventListener("click", Whenclick); // for remove this
+let input = document.querySelector("input");
+let datainput = function (val) {
+  if (val.data !== null) {
+    console.log(val.data);
+  }
+};
+input.addEventListener("input", datainput);
