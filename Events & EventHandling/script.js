83c4c7e2300067786ece14e5a016812d5184d04a
Shreeman Badatya <bshreemankumar@gmail.com>
2025-11-01 23:31:45 +0530

    Learning about Events & EventHandling
    addEventListener(“element name ” ,  function(){
         do some change....
    })
    
    1. click....
    2.dblclick....

---
+++ Events & EventHandling/script.js
@@ -0,0 +1,7 @@
+// let h1 = document.querySelector("h1");
+// function Whenclick() {
+//   h1.style.color = "red";
+// }
+// //h1.addEventListener("click", Whenclick);
+// h1.addEventListener("click", Whenclick);
+// //h1.removeEventListener("click", Whenclick); // for remove this
