13e46f7acc33da681f7e2aafbdbf32973f270a96
Shreeman Badatya <bshreemankumar@gmail.com>
2025-11-05 12:23:59 +0530

    mousemove event

--- Events & EventHandling/form/mouseEvents/script.js
+++ Events & EventHandling/form/mouseEvents/script.js
@@ -1,8 +1,13 @@
 let abcd = document.querySelector("#abcd");
 
-abcd.addEventListener("mouseover", () => {
-  abcd.style.backgroundColor = "green";
-});
-abcd.addEventListener("mouseout", () => {
-  abcd.style.backgroundColor = "red";
+// abcd.addEventListener("mouseover", () => {
+//   abcd.style.backgroundColor = "green";
+// });
+// abcd.addEventListener("mouseout", () => {
+//   abcd.style.backgroundColor = "red";
+// });
+
+window.addEventListener("mousemove", (details) => {
+  abcd.style.top = details.clientY + "px";
+  abcd.style.left = details.clientX + "px";
 });
