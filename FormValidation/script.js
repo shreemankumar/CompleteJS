f4d88c471e2a852623264ce60e217e763d714292
Shreeman Badatya <bshreemankumar@gmail.com>
2025-12-05 23:13:19 +0530

    updated

--- FormValidation/script.js
+++ FormValidation/script.js
@@ -1,12 +1,22 @@
-let form = document.querySelector("form");
-let nm = document.querySelector("#name");
+let form = document.querySelector("#form");
 
-form.addEventListener("submit", (det) => {
-  det.preventDefault();
-  let namelength = nm.value.length;
-  if (namelength <= 2) {
-    document.querySelector("#hide").style.display = "initial";
-  } else {
-    document.querySelector("#hide").style.display = "none";
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  let emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  let passwordRegex =
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+
+  let email = document.querySelector("#email").value;
+  let password = document.querySelector("#password").value;
+
+  if (!emailRegex.test(email)) {
+    alert("Invalid email format!");
+  }
+
+  if (!passwordRegex.test(password)) {
+    alert(
+      "Password must be 8+ chars, include uppercase, lowercase, number, and special character."
+    );
   }
 });
