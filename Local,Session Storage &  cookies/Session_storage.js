53447904c187f998c34db677cd46f1fbf222f4c5
Shreeman Badatya <bshreemankumar@gmail.com>
2025-12-31 18:40:53 +0530

    session storage

---
+++ Local,Session Storage &  cookies/Session_storage.js
@@ -0,0 +1,10 @@
+// session storage : Temp. storage
+//  by reopening tab, will be removing that data
+sessionStorage.setItem("age", "21");
+
+sessionStorage.getItem("age");
+
+sessionStorage.removeItem("age");
+
+sessionStorage.setItem("place", "hydearbad");
+sessionStorage.setItem("place", "berhampur");
