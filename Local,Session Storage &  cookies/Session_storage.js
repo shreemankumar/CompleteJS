8840d1df9ea75394ec1107e6dc91965f36b6eaf3
Shreeman Badatya <bshreemankumar@gmail.com>
2026-01-01 23:05:03 +0530

    JSON.stringify &  JSON.parse

--- Local,Session Storage &  cookies/Session_storage.js
+++ Local,Session Storage &  cookies/Session_storage.js
@@ -12,3 +12,17 @@ sessionStorage.setItem("place", "berhampur");
 // cookies : can handel small types of data (4 kb)
 // data goes automatically to server while reloading.
 document.cookie = "email = shreeman@gmail.com";
+// cookie name = email
+// value  =  shreeman@gmail.com
+
+// can view with the help of EditTheCookie chrome extension
+
+// local storage can always store srting value
+// To store arrays, objects, we have to use localStorage.
+
+let friends = ["ram", "shyam", "bulu"];
+localStorage.setItem("friends", JSON.stringify(friends));
+
+let friend = JSON.parse(localStorage.getItem("friends"));
+
+console.log(friend);
