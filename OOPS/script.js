f9228c30d164ec845529823a16067d6cb14f6334
Shreeman Badatya <bshreemankumar@gmail.com>
2026-01-03 01:31:57 +0530

    updated

--- OOPS/script.js
+++ OOPS/script.js
@@ -1,13 +1,20 @@
-function mobile(name, model, noOfCamera, fringerPrint, facelock) {
+function mobile(name, model, noOfCamera, fringerPrint, facelock, color) {
   (this.name = name),
     (this.model = model),
     (this.noOfCamera = noOfCamera),
     (this.fringerPrint = fringerPrint),
     (this.facelock = facelock);
+  this.color = color;
+  this.Add = function (text) {
+    let h1 = document.createElement("h1");
+    h1.textContent = text;
+    h1.style.color = color;
+    document.body.append(h1);
+  };
 }
 
-let mob1 = new mobile("samsung", "s20", 2, "yes", "yes");
-let mob2 = new mobile("apple", "17", 2, "nope", "yes");
+let mob1 = new mobile("samsung", "s20", 2, "yes", "yes", "black");
+let mob2 = new mobile("apple", "17", 2, "nope", "yes", "red");
 
 /**
  * In console
