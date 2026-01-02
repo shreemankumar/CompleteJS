31f9b3c83a1371c6910c2ce8d1db087a77f925b1
Shreeman Badatya <bshreemankumar@gmail.com>
2026-01-03 00:04:12 +0530

    this keyword

--- scope,Execution context & closures/script.js
+++ scope,Execution context & closures/script.js
@@ -29,3 +29,84 @@
  *
  * Manual binding  :  bind, call, apply
  */
+
+// this in global scope
+
+console.log(this); // window
+
+// this in functional scope
+
+function myFun() {
+  // window also
+  console.log(this);
+}
+myFun();
+
+// this in method
+// case 1
+
+let object1 = {
+  name: "name",
+  age: 1,
+
+  myFuns: function () {
+    console.log(this); // object1 it self
+    console.log(this.name); // name
+    console.log(this.age); // age
+  },
+};
+object1.myFuns();
+
+// case 2
+
+let object2 = {
+  name: "name",
+  age: 1,
+
+  myFuns: () => {
+    console.log(this); // window
+    console.log(this.name); // blank
+    console.log(this.age); // undefined
+  },
+};
+object2.myFuns();
+
+// case 3
+
+let object3 = {
+  name: "name",
+  age: 1,
+
+  myFun: function () {
+    let fn = () => {
+      console.log(this); // object1 it self
+      console.log(this.name); // name
+      console.log(this.age); // age
+    };
+    fn();
+  },
+};
+object3.myFun();
+
+// case 4
+
+let object4 = {
+  name: "name",
+  age: 1,
+
+  myFun: function () {
+    let fn = function () {
+      console.log(this); // window
+      console.log(this.name); // blank
+      console.log(this.age); // undefined
+    };
+    fn();
+  },
+};
+object4.myFun();
+
+// this in event handler
+let hOne = document.querySelector("h1");
+hOne.addEventListener("click", function () {
+  console.log((this.style.color = "red"));
+});
