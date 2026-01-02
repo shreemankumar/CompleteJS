a323f5e55680c153e9cfbbcfeb5968ce96dec409
Shreeman Badatya <bshreemankumar@gmail.com>
2026-01-02 23:43:57 +0530

    understanding tends to advance js concept

---
+++ scope,Execution context & closures/script.js
@@ -0,0 +1,31 @@
+// scope : functional , global , block scope
+
+// functional scope : within the funciton access:function(){//    }
+// global scope : access for all
+// block scope : within the { //    } curly braces
+
+// lexical scope vs dynamic scope
+
+/**
+ *
+ * closure definationa and how variable are preserved
+ * with advantages and dis advantages
+ *
+ * This keyword :  in,
+ *
+ * with 1 : global scope : window
+ *
+ *
+ * with 2 : functional scope : window
+ *
+ *
+ * with 3 : method(obj inside function(){} : obj  itself) and with arrow function () => {// window} but function(){() => {}} then : obj it self
+ *
+ *
+ * with 4 : event handeling : jiske uppre event hua hai ; this.style.color = "red";
+ *
+ *
+ * with 5 : class : blank obj
+ *
+ * Manual binding  :  bind, call, apply
+ */
