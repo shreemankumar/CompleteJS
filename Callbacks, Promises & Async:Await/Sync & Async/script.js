93fbe24ecd4228f14fb52ad784b9a43f8b59d4e8
Shreeman Badatya <bshreemankumar@gmail.com>
2026-01-03 15:49:42 +0530

    async syntex & uses

--- Callbacks, Promises & Async:Await/Sync & Async/script.js
+++ Callbacks, Promises & Async:Await/Sync & Async/script.js
@@ -14,3 +14,25 @@ setTimeout(() => {
   console.log("3");
 }, 4000);
 console.log("4");
+
+let pr = new Promise(function (res, rej) {
+  setTimeout(() => {
+    let result = Math.floor(Math.random() * 10);
+
+    if (result > 5) {
+      res("resolved with: " + result);
+    } else {
+      rej("rejected with: " + result);
+    }
+  }, 3000);
+});
+
+async function abcd() {
+  try {
+    let val = await pr;
+    console.log(val);
+  } catch (err) {
+    console.log(err);
+  }
+}
+abcd();
